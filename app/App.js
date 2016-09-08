fd5113154e5e73ad67a37e1cb0eec77705d567f6
Philip Lipman <philip.lipman@gmail.com>
2016-09-07 23:49:30 -0400

    updated

--- app/App.js
+++ app/App.js
@@ -1,21 +1,46 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
-
 /**
  * App Class
  */
 export default class App extends React.Component {
-  /**
-   * @param {number} param this is param.
-   * @return {number} this is return.
-   */
-  render() {
-    return (
-        <div>
-          Hello World Zoe Rules!
-        </div>
-    );
-  }
+
+    /**
+     * constructor
+     * @param {object} props - properties
+     */
+    constructor(props) {
+        super(props);
+
+        /**
+         * @type {object}
+         * @property {string} title - element title
+         */
+        this.state = {
+            title: props.title,
+        };
+    }
+
+    /**
+     * render
+     * @return {ReactElement} - generated markup
+     */
+    render() {
+        return (
+            <div>
+                {this.props.title}, this should be working!
+            </div>
+        );
+    }
 }
+
+App.propTypes = {
+    title: React.PropTypes.string,
+};
+
+App.defaultProps = {
+    title: 'Hello, World!',
+};
+
 ReactDom.render((<App />), document.getElementById('react-app'));
