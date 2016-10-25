a439d42a6881c4c17cd94b6b371dda4a1bd0831d
Philip Lipman <philip.lipman@gmail.com>
2016-10-24 23:18:12 -0400

    cleaning up code

--- app/__tests__/App.test.js
+++ app/__tests__/App.test.js
@@ -3,12 +3,12 @@ import TestUtils from 'react-addons-test-utils';
 
 import App from '../App.jsx';
 
-
 describe('App', () => {
     test('loads properly', () => {
         const component = TestUtils.renderIntoDocument( <App /> );
 
         expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+        expect(TestUtils.isCompositeComponentWithType(component, App)).toBeTruthy();
         expect(typeof component.props.title).toBe('string');
         expect(component.props.title).toBe('Express React App');
 
